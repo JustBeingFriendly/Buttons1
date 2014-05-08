0196d17132729c22ca244cbfbfb642b30270b529
JustBeingFriendly <srdenniston@gmail.com>
2014-05-08 14:09:10 +1200

    Buttons and SurfaceView rendering correctly. Buttons at 25% transparency. layout xml working correctly

--- Buttons1/gen/com/stephen/buttons1/R.java
+++ Buttons1/gen/com/stephen/buttons1/R.java
@@ -15,16 +15,20 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int animationView1=0x7f060001;
-        public static final int lbutton=0x7f060000;
+        public static final int animationView1=0x7f060000;
+        public static final int cbutton=0x7f060003;
+        public static final int lbutton=0x7f060002;
+        public static final int rbutton=0x7f060001;
     }
     public static final class layout {
         public static final int activity_buttons1=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int centre_button=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int left_button=0x7f040002;
+        public static final int right_button=0x7f040003;
     }
     public static final class style {
         /** 
